cb013fe896cf9e720a5da5be0537363de8be0358
Nikita Savelyev <nikita.savelyev@intel.com>
2023-09-07 10:01:09 +0200

    Added quantization

--- notebooks/250-music-generation/music_gen_quantization.py
+++ notebooks/250-music-generation/music_gen_quantization.py
@@ -1,8 +1,13 @@
+import datetime
+import pickle
 from collections import namedtuple
 import gc
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Optional, Tuple
 import warnings
+
+import onnx
 
 from IPython.display import Audio
 from openvino import Core, convert_model, PartialShape, save_model, Type
@@ -10,8 +15,15 @@ import numpy as np
 import soundfile as sf
 import torch
 from torch.jit import TracerWarning
+from tqdm import tqdm
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions, CausalLMOutputWithCrossAttentions
+
+import nncf
+import datasets
+
+from nncf import QuantizationPreset, ModelType
+from nncf.quantization.advanced_parameters import OverflowFix
 
 # Ignore tracing warnings
 warnings.filterwarnings("ignore", category=TracerWarning)
@@ -20,27 +32,50 @@ text_processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 
 SAMPLING_RATE = 32000
 SAMPLE_LENGTH = 8
-N_TOKENS = {8: 403}[SAMPLE_LENGTH]
+N_TOKENS = {2: 103, 4: 203, 8: 403}[SAMPLE_LENGTH]
 
 core = Core()
 DEVICE = "AUTO"
 
-models_dir = Path("./models")
+models_dir = Path(f"./models/{SAMPLE_LENGTH}")
 t5_ir_path = models_dir / "t5.xml"
 musicgen_0_ir_path = models_dir / "mg_0.xml"
 musicgen_ir_path = models_dir / "mg.xml"
 audio_decoder_onnx_path = models_dir / "encodec.onnx"
 audio_decoder_ir_path = models_dir / "encodec.xml"
 
+CALIBRATION_CACHE_PATH = "calibration_data/{}.pkl"
+
+
+COLLECT_CALIBRATION_DATA = False
+text_encoder_calibration_data = []
+text_decoder_0_calibration_data = []
+text_decoder_calibration_data = []
+audio_decoder_calibration_data = []
+
+
+@contextmanager
+def calibration_data_collection_context():
+    global COLLECT_CALIBRATION_DATA
+    COLLECT_CALIBRATION_DATA = True
+    yield
+    COLLECT_CALIBRATION_DATA = False
+
+
+text_decoder_inference_times = []
+
 
 class TextEncoderWrapper(torch.nn.Module):
     def __init__(self, encoder_ir, config):
         super().__init__()
-        self.encoder = core.compile_model(encoder_ir, DEVICE)
+        self.encoder = core.read_model(encoder_ir)
+        self.encoder_compiled = core.compile_model(self.encoder, DEVICE)
         self.config = config
 
     def forward(self, input_ids, **kwargs):
-        last_hidden_state = self.encoder(input_ids)[self.encoder.outputs[0]]
+        if COLLECT_CALIBRATION_DATA:
+            text_encoder_calibration_data.append(input_ids)
+        last_hidden_state = self.encoder_compiled(input_ids)[self.encoder_compiled.outputs[0]]
         last_hidden_state = torch.tensor(last_hidden_state)
         return BaseModelOutputWithPastAndCrossAttentions(last_hidden_state=last_hidden_state)
 
@@ -49,8 +84,10 @@ class MusicGenWrapper(torch.nn.Module):
     def __init__(self, music_gen_lm_0_ir, music_gen_lm_ir, config, num_codebooks, build_delay_pattern_mask,
                  apply_delay_pattern_mask):
         super().__init__()
-        self.music_gen_lm_0 = core.compile_model(music_gen_lm_0_ir, DEVICE)
-        self.music_gen_lm = core.compile_model(music_gen_lm_ir, DEVICE)
+        self.music_gen_lm_0 = core.read_model(music_gen_lm_0_ir)
+        self.music_gen_lm_0_compiled = core.compile_model(self.music_gen_lm_0, DEVICE)
+        self.music_gen_lm = core.read_model(music_gen_lm_ir)
+        self.music_gen_lm_compiled = core.compile_model(self.music_gen_lm, DEVICE)
         self.config = config
         self.num_codebooks = num_codebooks
         self.build_delay_pattern_mask = build_delay_pattern_mask
@@ -65,13 +102,21 @@ class MusicGenWrapper(torch.nn.Module):
             **kwargs
     ):
         if past_key_values is None:
-            model = self.music_gen_lm_0
+            model = self.music_gen_lm_0_compiled
             arguments = (input_ids, encoder_hidden_states, encoder_attention_mask)
+            if COLLECT_CALIBRATION_DATA:
+                text_decoder_0_calibration_data.append(arguments)
         else:
-            model = self.music_gen_lm
+            model = self.music_gen_lm_compiled
             arguments = (input_ids, encoder_hidden_states, encoder_attention_mask, *past_key_values)
+            if COLLECT_CALIBRATION_DATA:
+                text_decoder_calibration_data.append(arguments)
 
+        start_time = datetime.datetime.now()
         output = model(arguments)
+        end_time = datetime.datetime.now()
+        if past_key_values is not None:
+            text_decoder_inference_times.append((end_time - start_time).total_seconds())
         return CausalLMOutputWithCrossAttentions(
             logits=torch.tensor(output[model.outputs[0]]),
             past_key_values=tuple([output[model.outputs[i]] for i in range(1, 97)]),
@@ -81,27 +126,47 @@ class MusicGenWrapper(torch.nn.Module):
 class AudioDecoderWrapper(torch.nn.Module):
     def __init__(self, decoder_ir, config):
         super().__init__()
-        self.decoder = core.compile_model(decoder_ir, DEVICE)
+        self.decoder = core.read_model(decoder_ir)
+        self.decoder_compiled = core.compile_model(self.decoder, DEVICE)
         self.config = config
         self.output_type = namedtuple("AudioDecoderOutput", ["audio_values"])
 
     def decode(self, output_ids, audio_scales):
-        output = self.decoder(output_ids)[self.decoder.outputs[0]]
+        if COLLECT_CALIBRATION_DATA:
+            audio_decoder_calibration_data.append(output_ids)
+        output = self.decoder_compiled(output_ids)[self.decoder_compiled.outputs[0]]
         return self.output_type(audio_values=torch.tensor(output))
 
 
-def infer_pytorch_model(model, input_text, device="cpu"):
+class AudioDecoder(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, output_ids):
+        return self.model.decode(output_ids, [None])
+
+
+def infer_model(model, input_text, save_filename, device="cpu"):
+    global text_decoder_inference_times
     assert SAMPLE_LENGTH * model.config.audio_encoder.frame_rate + 3 == N_TOKENS
     assert model.config.audio_encoder.sampling_rate == SAMPLING_RATE
+
+    print("Inferencing")
 
     inputs = text_processor(
         text=[input_text],
         return_tensors="pt",
     )
 
+    start_time = datetime.datetime.now()
+    text_decoder_inference_times = []
     audio_values = model.generate(**inputs.to(device), do_sample=True, guidance_scale=3, max_new_tokens=N_TOKENS)
+    print(f"Decoder inference time: {sum(text_decoder_inference_times):.2f} "
+          f"({len(text_decoder_inference_times)} calls)")
+    print(f"Total inference time: {(datetime.datetime.now() - start_time).total_seconds():.2f}")
 
-    sf.write("original.wav", audio_values[0, 0].cpu().numpy(), samplerate=SAMPLING_RATE)
+    sf.write(save_filename, audio_values[0, 0].cpu().numpy(), samplerate=SAMPLING_RATE)
 
 
 def convert_to_ir(model):
@@ -161,20 +226,10 @@ def convert_to_ir(model):
     # 3. Convert Audio Decoder
     #
     if not audio_decoder_onnx_path.exists():
-        class AudioDecoder(torch.nn.Module):
-            def __init__(self, model):
-                super().__init__()
-                self.model = model
-
-            def forward(self, output_ids):
-                return self.model.decode(output_ids, [None])
-
-        audio_decoder_input = {'output_ids': torch.ones(1, 1, 4, N_TOKENS - 3, dtype=torch.int64), }
-
         with torch.no_grad():
             torch.onnx.export(
                 model=AudioDecoder(model.audio_encoder),
-                args=audio_decoder_input,
+                args={'output_ids': torch.ones(1, 1, 4, N_TOKENS - 3, dtype=torch.int64), },
                 f=audio_decoder_onnx_path,
                 input_names=['output_ids', ],
                 output_names=['decoded_audio'],
@@ -193,7 +248,7 @@ def convert_to_ir(model):
         gc.collect()
 
 
-def infer_ov_model(model, input_text):
+def convert_to_ov_model(model):
     # Embedding the converted models into the original pipeline
     text_encode_ov = TextEncoderWrapper(t5_ir_path, model.text_encoder.config)
     musicgen_decoder_ov = MusicGenWrapper(
@@ -214,20 +269,196 @@ def infer_ov_model(model, input_text):
     model.text_encoder = text_encode_ov
     model.decoder = musicgen_decoder_ov
     model.audio_encoder = audio_encoder_ov
+    return model
 
-    inputs = text_processor(
-        text=[input_text],
-        return_tensors="pt",
+
+def collect_calibration_data(n_samples):
+    global text_encoder_calibration_data, text_decoder_0_calibration_data, text_decoder_calibration_data, \
+        audio_decoder_calibration_data
+
+    calibration_cache_path = Path(CALIBRATION_CACHE_PATH.format(n_samples))
+
+    calibration_dataset = datasets.load_dataset("google/MusicCaps", split="train", streaming=True).take(
+        n_samples)
+    if not calibration_cache_path.exists():
+        if not calibration_cache_path.parent.exists():
+            calibration_cache_path.parent.mkdir(parents=True)
+        with calibration_data_collection_context():
+            for data_item in tqdm(calibration_dataset, total=n_samples,
+                                  desc="Collecting calibration data"):
+                inputs = text_processor(text=[data_item["caption"]], return_tensors="pt").to("cpu")
+                model.generate(**inputs, do_sample=True, guidance_scale=3, max_new_tokens=N_TOKENS)
+
+        # with open(calibration_cache_path, 'wb') as f:
+        #     pickle.dump((text_encoder_calibration_data, text_decoder_0_calibration_data, text_decoder_calibration_data,
+        #                  audio_decoder_calibration_data), f)
+    else:
+        print("Loading calibration data")
+        with open(calibration_cache_path, "rb") as f:
+            text_encoder_calibration_data, text_decoder_0_calibration_data, text_decoder_calibration_data, \
+                audio_decoder_calibration_data = pickle.load(f)
+
+
+def quantize(model, save_dir, n_calibration_samples, preset, sq_alpha):
+    save_dir = Path(save_dir)
+
+    #
+    # Collect calibration data
+    #
+
+    text_encode_ov = TextEncoderWrapper(t5_ir_path, model.text_encoder.config)
+    musicgen_decoder_ov = MusicGenWrapper(
+        musicgen_0_ir_path,
+        musicgen_ir_path,
+        model.decoder.config,
+        model.decoder.num_codebooks,
+        model.decoder.build_delay_pattern_mask,
+        model.decoder.apply_delay_pattern_mask
     )
+    audio_encoder_ov = AudioDecoderWrapper(audio_decoder_ir_path, model.audio_encoder.config)
+    model.text_encoder = text_encode_ov
+    model.decoder = musicgen_decoder_ov
+    model.audio_encoder = audio_encoder_ov
 
-    audio_values = model.generate(**inputs.to("cpu"), do_sample=True, guidance_scale=3, max_new_tokens=N_TOKENS)
+    #
+    # Quantize text encoder
+    #
+    # compressed_text_encoder_path = save_dir / "text_encoder.xml"
+    # if not compressed_text_encoder_path.exists():
+    #     compressed_text_encoder = nncf.quantize(
+    #         model.text_encoder.encoder,
+    #         calibration_dataset=nncf.Dataset(text_encoder_calibration_data),
+    #         preset=QuantizationPreset.PERFORMANCE,
+    #         subset_size=len(text_encoder_calibration_data),
+    #         model_type=ModelType.TRANSFORMER,
+    #         ignored_scope=None,
+    #         advanced_parameters=nncf.AdvancedQuantizationParameters(
+    #             overflow_fix=OverflowFix.DISABLE,
+    #             smooth_quant_alpha=0.95
+    #         )
+    #     )
+    #     save_model(compressed_text_encoder, compressed_text_encoder_path)
+    # model.text_encoder = TextEncoderWrapper(compressed_text_encoder_path, model.text_encoder.config)
 
-    sf.write("openvino.wav", audio_values[0, 0].cpu().numpy(), samplerate=SAMPLING_RATE)
+    #
+    # Quantize text decoder 0
+    #
+    # compressed_text_decoder_0_path = save_dir / "text_decoder_0.xml"
+    # if not compressed_text_decoder_0_path.exists():
+    #     compressed_text_decoder_0 = nncf.quantize(
+    #         model.decoder.music_gen_lm_0,
+    #         calibration_dataset=nncf.Dataset(text_decoder_0_calibration_data),
+    #         preset=QuantizationPreset.MIXED,
+    #         subset_size=len(text_decoder_0_calibration_data),
+    #         model_type=ModelType.TRANSFORMER,
+    #         ignored_scope=None,
+    #         advanced_parameters=nncf.AdvancedQuantizationParameters(
+    #             overflow_fix=OverflowFix.DISABLE,
+    #             smooth_quant_alpha=0.95
+    #         )
+    #     )
+    #     save_model(compressed_text_decoder_0, compressed_text_decoder_0_path)
+    # model.decoder.music_gen_lm_0 = core.read_model(compressed_text_decoder_0_path)
+    # model.decoder.music_gen_lm_0_compiled = core.compile_model(model.decoder.music_gen_lm_0, DEVICE)
+
+    #
+    # Quantize text decoder full
+    #
+    compressed_text_decoder_path = save_dir / "text_decoder.xml"
+    if not compressed_text_decoder_path.exists():
+        collect_calibration_data(n_calibration_samples)
+        print("Quantizing")
+        compressed_text_decoder = nncf.quantize(
+            model.decoder.music_gen_lm,
+            calibration_dataset=nncf.Dataset(text_decoder_calibration_data),
+            preset=preset,
+            subset_size=len(text_decoder_calibration_data),
+            model_type=ModelType.TRANSFORMER,
+            ignored_scope=None,
+            advanced_parameters=nncf.AdvancedQuantizationParameters(
+                overflow_fix=OverflowFix.DISABLE,
+                smooth_quant_alpha=sq_alpha
+            )
+        )
+
+        # print("Compressing weights")
+        # compressed_text_decoder = nncf.compress_weights(model.decoder.music_gen_lm)
+
+        save_model(compressed_text_decoder, compressed_text_decoder_path)
+    model.decoder.music_gen_lm = core.read_model(compressed_text_decoder_path)
+    model.decoder.music_gen_lm_compiled = core.compile_model(model.decoder.music_gen_lm, DEVICE)
+
+    #
+    # Quantize audio decoder
+    #
+    # compressed_audio_decoder_path = save_dir / "audio_decoder.xml"
+    # if not compressed_audio_decoder_path.exists():
+    #     compressed_audio_decoder = nncf.quantize(
+    #         model.audio_encoder.decoder,
+    #         calibration_dataset=nncf.Dataset(audio_decoder_calibration_data),
+    #         preset=QuantizationPreset.MIXED,
+    #         subset_size=len(audio_decoder_calibration_data),
+    #         ignored_scope=None,
+    #         advanced_parameters=nncf.AdvancedQuantizationParameters(
+    #             overflow_fix=OverflowFix.DISABLE,
+    #             disable_channel_alignment=False
+    #         )
+    #     )
+    #     save_model(compressed_audio_decoder, compressed_audio_decoder_path)
+    # model.audio_encoder.decoder = core.read_model(compressed_audio_decoder_path)
+    # model.audio_encoder.decoder_compiled = core.compile_model(model.audio_encoder.decoder, DEVICE)
+
+    #
+    # Quantize audio decoder through ONNX
+    #
+    # compressed_audio_decoder_path = save_dir / "audio_decoder.xml"
+    # if not compressed_audio_decoder_path.exists():
+    #     audio_decoder_calibration_data_onnx = [dict(output_ids=it.numpy()) for it in audio_decoder_calibration_data]
+    #     compressed_audio_decoder_onnx = nncf.quantize(
+    #         onnx.load(audio_decoder_onnx_path),
+    #         calibration_dataset=nncf.Dataset(audio_decoder_calibration_data_onnx),
+    #         preset=QuantizationPreset.MIXED,
+    #         subset_size=len(audio_decoder_calibration_data_onnx),
+    #         ignored_scope=None,
+    #         advanced_parameters=nncf.AdvancedQuantizationParameters(
+    #             overflow_fix=OverflowFix.DISABLE,
+    #             disable_channel_alignment=False
+    #         )
+    #     )
+    #     onnx.save(compressed_audio_decoder_onnx, compressed_audio_decoder_path.with_suffix(".onnx"))
+    #     compressed_audio_decoder_ov = convert_model(compressed_audio_decoder_path.with_suffix(".onnx"))
+    #     save_model(compressed_audio_decoder_ov, compressed_audio_decoder_path)
+    # model.audio_encoder.decoder = core.read_model(compressed_audio_decoder_path)
+    # model.audio_encoder.decoder_compiled = core.compile_model(model.audio_encoder.decoder, DEVICE)
+
+    return model
 
 
 model = MusicgenForConditionalGeneration.from_pretrained(
     "facebook/musicgen-small", torchscript=True, return_dict=False).to("cpu").eval()
-# infer_pytorch_model(model, "80s pop track with bassy drums and synth")
-# convert_to_ir(model)
-infer_ov_model(model, "80s pop track with bassy drums and synth")
+# print("tokens:", SAMPLE_LENGTH * model.config.audio_encoder.frame_rate + 3)
 
+# infer_model(model, "80s pop track with bassy drums and synth", "original.wav")
+
+# convert_to_ir(model)
+
+model = convert_to_ov_model(model)
+
+# Total: 31 sec
+# Decoder: 26 sec (N_TOKENS - 1 calls)
+# infer_model(model, "80s pop track with bassy drums and synth", f"openvino_{SAMPLE_LENGTH}.wav")
+
+save_dir = Path("quantized/4_6/mixed_sq.15")
+model = quantize(model,
+                 save_dir=save_dir,
+                 n_calibration_samples=6,
+                 preset=QuantizationPreset.MIXED,
+                 sq_alpha=0.15)
+
+# PTQ:
+# Total: 25 sec
+# Decoder: 20 sec (N_TOKENS - 1 calls)
+# WC:
+# Total: 30
+# Decoder: 25
+infer_model(model, "80s pop track with bassy drums and synth", save_dir / "result_4.wav")
