8b72befb819278e092424ca95cc7a08dcb09a346
Nikita Savelyev <nikita.savelyev@intel.com>
2023-12-04 14:42:00 +0100

    Implemented SQNR based calibration

--- fp16_calibration/partially_upcast_nodes_to_fp32.py
+++ fp16_calibration/partially_upcast_nodes_to_fp32.py
@@ -32,12 +32,14 @@ def get_thresholds_per_op():
 
 def partially_upcast_nodes_to_fp32(orig_model: Model, example_input: Union[List, Dict], half_type: str,
                                    batch_size: int = -1, thresholds_per_op: Dict[str, Tuple] = None,
+                                   upcast_ratio: float = 0.1,
                                    verbose: bool = False) -> Model:
     assert half_type in ("f16", "bf16")
     device = "GPU" if half_type == "f16" else "CPU"
 
     nodes_to_track_names = get_nodes_to_track(orig_model)
-    nodes_with_errors_names = []
+    # node_to_upcast_names = []
+    nodes_sqnrs = []
     batch_size = len(nodes_to_track_names) if batch_size == -1 else batch_size
     for i in tqdm(range(0, len(nodes_to_track_names), batch_size), disable=not verbose):
         model = orig_model.clone()
@@ -51,15 +53,25 @@ def partially_upcast_nodes_to_fp32(orig_model: Model, example_input: Union[List,
         fp16_infer_values_batch = infer_nodes(nodes_to_track_batch, fp16_full_net_infer_values_batch, device, half_type)
         fp32_infer_values_batch = infer_nodes(nodes_to_track_batch, fp16_full_net_infer_values_batch, device, "f32")
 
-        nodes_with_errors_names_batch = get_nodes_with_errors(nodes_to_track_batch, fp16_infer_values_batch,
-                                                              fp32_infer_values_batch, thresholds_per_op, verbose)
-        nodes_with_errors_names.extend(nodes_with_errors_names_batch)
+        # node_to_upcast_names_batch = get_nodes_with_errors(nodes_to_track_batch, fp16_infer_values_batch,
+        #                                                       fp32_infer_values_batch, thresholds_per_op, verbose)
+        # node_to_upcast_names.extend(node_to_upcast_names_batch)
+        nodes_sqnrs.extend(get_nodes_sqnrs(nodes_to_track_batch, fp16_infer_values_batch, fp32_infer_values_batch))
 
         del fp16_full_net_infer_values_batch, fp16_infer_values_batch, fp32_infer_values_batch, model, name_to_node_map
         gc.collect()
 
+    node_names = [it[0] for it in nodes_sqnrs]
+    node_sqnrs = np.array([it[1] for it in nodes_sqnrs], dtype=np.float32)
+    sqnr_quantile = np.quantile(node_sqnrs, upcast_ratio)
+    node_to_upcast_names = [node_names[i] for i in np.where(node_sqnrs <= sqnr_quantile)[0]]
+    if verbose:
+        print(f"SQNR {upcast_ratio:.2f}-quantile equals {sqnr_quantile:.2f}. "
+              f"Upcasted {len(node_to_upcast_names)} of {len(node_names)} considered nodes:")
+        print("\n".join(node_to_upcast_names))
+
     new_model = orig_model.clone()
-    mark_nodes_to_upcast_to_fp32(new_model, nodes_with_errors_names)
+    mark_nodes_to_upcast_to_fp32(new_model, node_to_upcast_names)
     return new_model
 
 
@@ -174,6 +186,14 @@ def get_nodes_with_errors(nodes: List[Node], fp16_infer_vals: List, fp32_infer_v
     return nodes_with_errors
 
 
+def get_nodes_sqnrs(nodes: List[Node], fp16_infer_vals: List, fp32_infer_vals: List) -> List[Tuple[str, float]]:
+    nodes_sqnrs = []
+    for node, fp16_val, fp32_val in zip(nodes, fp16_infer_vals, fp32_infer_vals):
+        sqnr = compute_sqnr(fp32_val, fp16_val)
+        nodes_sqnrs.append((node.get_friendly_name(), sqnr))
+    return nodes_sqnrs
+
+
 def is_model_partially_upcasted(model) -> bool:
     for node in model.get_ordered_ops():
         if node.get_type_name() not in ops_to_track_map.keys():
@@ -187,6 +207,17 @@ def mark_nodes_to_upcast_to_fp32(model: ov.Model, nodes_with_errors: List[str]) 
     for node in model.get_ordered_ops():
         if node.get_friendly_name() in nodes_with_errors:
             node.get_rt_info()[rt_info_name_to_keep_orig_precision()] = ''
+
+
+def compute_sqnr(x, y):
+    # x -- original, y -- quantized
+
+    x = np.nan_to_num(x, posinf=np.finfo(x.dtype).max)
+    y = np.nan_to_num(y, posinf=np.finfo(y.dtype).max)
+
+    Ps = np.linalg.norm(x)
+    Pn = np.nan_to_num(np.linalg.norm(x - y), posinf=np.finfo(np.float32).max)
+    return 20 * np.log10(Ps / Pn)
 
 
 def compare_tensors(node: Node, a: np.ndarray, b: np.ndarray, new_thresholds_per_op, verbose: bool = False) -> bool:
@@ -212,20 +243,22 @@ def compare_tensors(node: Node, a: np.ndarray, b: np.ndarray, new_thresholds_per
     rel_tol = thresholds[2]
 
     rel_diff_ratio = np.size(np.where(rel_error >= rel_threshold)) / out_size
-    diff_too_large = mean_rel_error > rel_tol
-    # if mean_rel_error < rel_tol:
-    #     return False
-    if rel_diff_ratio > rel_threshold_ratio:
+    result = False
+    if not(mean_rel_error < rel_tol) and rel_diff_ratio > rel_threshold_ratio:  # "not (...)" due to nans
         if verbose:
             print(f'Upcasted node {node.get_friendly_name()} with {rel_threshold:.2f} '
                   f'rel2_diff_ratio {rel_diff_ratio:.6f} and mean_rel_error {mean_rel_error:.6f}')
-        diff_too_large &= True
+        result = True
 
     # node_name = node.get_friendly_name().replace('/', '%')
     # from pathlib import Path
-    # filepath_fp16 = Path(f"activations/{node_name}_fp16_{int(mean_rel_error > rel_tol)}_{int(rel_diff_ratio > rel_threshold_ratio)}.npy")
-    # filepath_fp32 = Path(f"activations/{node_name}_fp32_{int(mean_rel_error > rel_tol)}_{int(rel_diff_ratio > rel_threshold_ratio)}.npy")
+    # outcome1 = int(not(mean_rel_error < rel_tol))
+    # outcome2 = int(rel_diff_ratio > rel_threshold_ratio)
+    # save_dir = Path("activations/tiny-sd-unet")
+    # save_dir.mkdir(exist_ok=True)
+    # filepath_fp16 = save_dir / f"{node_name}_fp16_{outcome1}_{outcome2}.npy"
+    # filepath_fp32 = save_dir / f"{node_name}_fp32_{outcome1}_{outcome2}.npy"
     # np.save(filepath_fp16, a)
     # np.save(filepath_fp32, b)
 
-    return diff_too_large
+    return result
