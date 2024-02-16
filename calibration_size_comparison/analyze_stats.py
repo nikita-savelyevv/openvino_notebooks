12a2da7cc421b9c9f2818b98e494fcfa4294c525
Nikita Savelyev <nikita.savelyev@intel.com>
2024-02-16 15:19:45 +0100

    IWP

--- calibration_size_comparison/analyze_stats.py
+++ calibration_size_comparison/analyze_stats.py
@@ -29,21 +29,24 @@ for size, acc in zip(xs, ys):
                 for stat_name in ["min_values", "max_values"]:
                     stat_value = stat_dict[stat_name]
                     stat_value_min = stat_value.min()
-                    stat_value_max = stat_value.max()
-                    stat_value_mean = stat_value.mean()
-                    stat_value_std = stat_value.std()
+                    # stat_value_max = stat_value.max()
+                    # stat_value_diff = stat_value_max - stat_value_min
+                    # stat_value_mean = stat_value.mean()
+                    # stat_value_std = stat_value.std()
                     x.extend([
                         stat_value_min,
-                        stat_value_max,
-                        stat_value_mean,
-                        stat_value_std
+                        # stat_value_max,
+                        # stat_value_diff,
+                        # stat_value_mean,
+                        # stat_value_std
                     ])
                     if X is None:
                         legend.extend([
                             f"{node_name}_{stat_name}_min",
-                            f"{node_name}_{stat_name}_max",
-                            f"{node_name}_{stat_name}_mean",
-                            f"{node_name}_{stat_name}_std",
+                            # f"{node_name}_{stat_name}_max",
+                            # f"{node_name}_{stat_name}_diff",
+                            # f"{node_name}_{stat_name}_mean",
+                            # f"{node_name}_{stat_name}_std",
                         ])
     x = np.array(x, dtype=np.float32)
     if not isinstance(X, np.ndarray):
@@ -51,11 +54,20 @@ for size, acc in zip(xs, ys):
     else:
         X = np.append(X, x[None], axis=0)
 
+# min/max/mean/std indices
 # ind = 228
 # ind = 249
 # ind = 568
 # ind = 569
 # ind = 570
+
+# min/max/diff
+# ind = 171
+# ind = 187
+# ind = 426
+# ind = 427
+
+# ind = 142
 # plt.scatter(X[:, ind], ys)
 # plt.xlabel(legend[ind])
 # plt.ylabel('Accuracy')
@@ -64,7 +76,7 @@ for size, acc in zip(xs, ys):
 # exit(0)
 
 reg = make_pipeline(StandardScaler(),
-                    SGDRegressor(max_iter=1000, tol=1e-3, verbose=1, eta0=1e-3))
+                    SGDRegressor(max_iter=1000, tol=1e-5, verbose=1, eta0=1e-3))
 reg.fit(X, ys)
 
 coef = reg[1].coef_
